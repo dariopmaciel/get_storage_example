339496dd7d71bfb3b7ee8121ebb222e7a18ef340
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-11-12 09:22:11 -0300

    ReadWriteValue do getstorage via get  e setters

--- lib/core/storage/storage.dart
+++ lib/core/storage/storage.dart
@@ -1,7 +1,17 @@
-
 import 'package:get_storage/get_storage.dart';
 
 class Storage {
-  final name = ReadWriteValue('nameKey', '');
-  final nameGetX = ReadWriteValue('nameKey', '', () => GetStorage('JornadaGetX'),);
+  // final name = ReadWriteValue('nameKey', '');
+  final _name = ReadWriteValue('nameKey', '');
+  // final nameGetX = ReadWriteValue('nameKey', '', () => GetStorage('JornadaGetX'),);
+  final _nameGetX =
+      ReadWriteValue('nameKey', '', () => GetStorage('JornadaGetX'));
+
+
+  set name(String value)=> _name.val = value;
+  String get name => _name.val;
+
+  set nameGetX(String value)=> _nameGetX.val = value;
+  String get nameGetX => _nameGetX.val;
+
 }
--- lib/pages/storage/storage_page.dart
+++ lib/pages/storage/storage_page.dart
@@ -23,33 +23,39 @@ class _StoragePageState extends State<StoragePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Container padrão: ${storage.name.val}'),
+            // Text('Container padrão: ${storage.name.val}'),
+            Text('Container padrão: ${storage.name}'),
             ElevatedButton(
               onPressed: () {
-                storage.name.val = 'Dario P MAciel';
+                // storage.name.val = 'Dario P MAciel';
+                storage.name = 'Dario P MAciel';
                 setState(() {});
               },
               child: const Text('Gravar Nome'),
             ),
             ElevatedButton(
               onPressed: () {
-                storage.name.val = '';
+                // storage.name.val = '';
+                storage.name = '';
                 setState(() {});
               },
               child: const Text('Remover Nome'),
             ),
             const SizedBox(height: 30), //!------------------
-            Text('Container segundario: ${storage.nameGetX.val}'),
+            // Text('Container segundario: ${storage.nameGetX.val}'),
+            Text('Container segundario: ${storage.nameGetX}'),
             ElevatedButton(
               onPressed: () {
-                storage.nameGetX.val = 'Dario P MAciel - GetX';
+                // storage.nameGetX.val = 'Dario P MAciel - GetX';
+                storage.nameGetX = 'Dario P MAciel - GetX';
                 setState(() {});
               },
               child: const Text('Gravar Nome'),
             ),
             ElevatedButton(
               onPressed: () {
-                storage.nameGetX.val = '';
+                // storage.nameGetX.val = '';
+                storage.nameGetX  = '';
                 setState(() {});
               },
               child: const Text('Remover Nome'),
